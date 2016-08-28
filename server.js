0ef37caca20cf8de7756548a55a7e57f53627918
Daniel Lim <hbt@markhamboards.com>
2016-08-27 22:53:53 -0400

    server - public

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ app.set('port', (process.env.PORT || 5000));
 // process.env.PORT lets the port be set by Heroku
 // var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/');
 
 // set the view engine to php
 // app.set('view engine', 'php');
@@ -21,13 +21,6 @@ app.get('/', function(request, response) {
 
 // make express look in the public directory for assets (css/js/img)
 // app.use(express.static(__dirname + '/'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    // res.render('index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
