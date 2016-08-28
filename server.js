2b3a74c7b5c0cc0a40c610ed36431fdd026387d7
Daniel Lim <hbt@markhamboards.com>
2016-08-27 22:10:23 -0400

    + server

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
