dc295f96f0a6024cc1f8a0b8ac1338db00b8d73d
Daniel Lim <hbt@markhamboards.com>
2016-08-27 22:06:30 -0400

    into node app

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
+// views is directory for all template files
+app.set('views', __dirname + '/');
+
+// set the view engine to php
 app.set('view engine', 'php');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route  
+// set the home page route
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
